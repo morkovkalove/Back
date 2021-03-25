5c075e2ed48658f8b91cd84fb11005e8dc69fc7a
morkovkalove <73894426+morkovkalove@users.noreply.github.com>
2021-03-26 00:16:20 +0300

    Add files via upload

---
+++ calcul.py
@@ -0,0 +1,30 @@
+#simple calcul
+from colorama import init
+from colorama import Fore, Back, Style
+
+# use Colorama to make Termcolor work on Windows too
+init()
+
+print( Fore.BLACK)
+print( Back.MAGENTA)
+
+what = input("Что вычисляем? (+, -): ")
+
+print( Back.CYAN)
+
+a = float(input ("Введи первое число: "))
+b = float(input ("Введи второе число: "))
+
+print( Back.YELLOW)
+
+if what == "+":
+    c = a + b
+    print("Результат: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат: " + str(c))
+else:
+    print("Выбрано неправильное действие!")    
+
+
+
